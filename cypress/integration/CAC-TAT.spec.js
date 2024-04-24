0039ad70e1e3e9e1cdbd065666f752cd6ad617b3
cadufelipe <felipecarloscontato@gmail.com>
2024-04-23 21:05:56 -0300

    verificação title

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,10 +2,9 @@
 
 describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
-        cy.visit('./scr/index.html')
+        cy.visit('./src/index.html')
 
-        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
-
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
   })
   
